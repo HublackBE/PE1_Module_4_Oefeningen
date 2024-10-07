965e56fd5ece90601dd147f24b51ac247e2c1b3d
Hublack <71221641+HublackBE@users.noreply.github.com>
2024-10-07 12:02:32 +0200

    h2_schaar_steen_papier done

---
+++ h2_schaar_steen_papier.mjs
@@ -0,0 +1,43 @@
+import * as readline from 'node:readline/promises';
+import {stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface(input, output);
+
+let scoreAI = 0;
+let scoreUser = 0;
+
+let choices = ['rock', 'paper', 'scissors'];
+
+let computerChoice;
+let userChoice;
+
+let win = new Map([
+    ['rock', 'scissors'],
+    ['paper', 'rock'],
+    ['scissors', 'paper']
+]);
+
+while(scoreUser < 3 && scoreAI < 3){
+    computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    userChoice = (await userInput.question('\nKies uit rock, paper en scissors: ')).toLowerCase();
+    if(!choices.includes(userChoice)){
+        console.log('\nFoute input.');
+    }else if(userChoice === win.get(computerChoice)){
+        console.log('\nSorry, je bent verloren. De computer had', computerChoice);
+        scoreAI++;
+    }else if(userChoice === computerChoice){
+        console.log('\nGelijke stand! De computer had ook', userChoice);
+    }else{
+        console.log('\nJe bent gewonnen! De computer had', computerChoice);
+        scoreUser++;
+    }
+    console.log('\n\nUser:', scoreUser, '|', 'Computer:', scoreAI);
+}
+
+if(scoreUser >= 3){
+    console.log('\nJe bent het spel gewonnen!')
+}else{
+    console.log('\nSorry, je bent het spel verloren.')
+}
+
+
+process.exit();
